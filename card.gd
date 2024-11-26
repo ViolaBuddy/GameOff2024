8d68db43b3582634471e5949f0b204f14b4040d0
Benny Johnson <Benny.Johnson@jhuapl.edu>
2024-11-26 15:10:26 -0500

    Fixed reinstantiation on every loop, Added selection system

--- card.gd
+++ card.gd
@@ -1,7 +1,8 @@
-extends Node2D
+class_name Card extends Node2D
 
 var faceUp := true
 var nameText := ""
+var isSelected := false
 
 func _ready() -> void:
 	flipFaceUp()
@@ -21,6 +22,18 @@ func flipFaceDown() -> void:
 	faceUp = false
 	$CardSprite.z_index = -1
 	$CardBackSprite.z_index = 1
+	
+func toggleSelect() -> bool:
+	isSelected = !isSelected
+	
+	if isSelected: 
+		$CardSprite.scale *= 1.1
+		print("Increasing scale")
+	else:
+		$CardSprite.scale *= (1.0 / 1.1)
+		print("Decreasing scale")
+		
+	return isSelected
 
 func setText(newText:String) -> void:
 	nameText = newText
--- main.gd
+++ main.gd
@@ -20,38 +20,25 @@ var board_state = [] #2d array: board_state[row][column]
 
 const HAND_SIZE = 3
 
-const BOARD_WIDTH = 5
-const BOARD_HEIGHT = 5
-const CARD_WIDTH = 240
-const CARD_HEIGHT = 336
-const BOARD_SCALE = 0.5
-const SPACE_BETWEEN_CARDS = 10
-const BOARD_OFFSET_X = -((CARD_WIDTH*BOARD_SCALE+SPACE_BETWEEN_CARDS)*BOARD_WIDTH) / 2  + CARD_WIDTH*BOARD_SCALE/2
-const BOARD_OFFSET_Y = -((CARD_HEIGHT*BOARD_SCALE+SPACE_BETWEEN_CARDS)*BOARD_HEIGHT) / 2 + CARD_HEIGHT*BOARD_SCALE/2
+const BOARD_WIDTH := 5
+const BOARD_HEIGHT := 5
+const CARD_WIDTH_PX := 240
+const CARD_HEIGHT_PX := 336
+
+const BOARD_SCALE := 0.5
+const SPACE_BETWEEN_CARDS_PX := 10
+const BOARD_OFFSET_X = -((CARD_WIDTH_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*BOARD_WIDTH) / 2  + CARD_WIDTH_PX*BOARD_SCALE/2
+const BOARD_OFFSET_Y = -((CARD_HEIGHT_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*BOARD_HEIGHT) / 2 + CARD_HEIGHT_PX*BOARD_SCALE/2
 
 @onready var instancedCard = preload("res://Card.tscn")
 @onready var Hand = $Hand
 @onready var Preview = $Preview
 
 func _ready() -> void:
-	# create board
-	for r in range(BOARD_HEIGHT):
-		board_state.append([])
-		for c in range(BOARD_WIDTH):
-			var myCard = instancedCard.instantiate()
-			board_state[r].append(myCard)
-			$Board.add_child(myCard)
-			myCard.position = Vector2(
-				(CARD_WIDTH*BOARD_SCALE+SPACE_BETWEEN_CARDS)*c + BOARD_OFFSET_X,
-				(CARD_HEIGHT*BOARD_SCALE+SPACE_BETWEEN_CARDS)*r + BOARD_OFFSET_Y)
-			myCard.scale = Vector2(BOARD_SCALE, BOARD_SCALE)
-			myCard.flipFaceDown()
-	
 	# set up deck
 	deck.shuffle()
 	for i in range(HAND_SIZE):
-		draw_card()
-	update_hand_visualization()
+		draw_from_deck()
 	
 	# set up preview
 	var previewClickArea = Preview.get_node("ClickArea")
@@ -59,9 +46,22 @@ func _ready() -> void:
 	previewClickArea.queue_free()
 	Preview.flipFaceDown()
 	
+	# Initialize Board
+	for r in range(BOARD_HEIGHT):
+		board_state.append([])
+		for c in range(BOARD_WIDTH):
+			var myCard = instancedCard.instantiate()
+			board_state[r].append(myCard)
+			$Board.add_child(myCard)
+			myCard.position = Vector2(
+				(CARD_WIDTH_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*c + BOARD_OFFSET_X,
+				(CARD_HEIGHT_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*r + BOARD_OFFSET_Y)
+			myCard.scale = Vector2(1.0, 1.0) * BOARD_SCALE
+	
 func _process(delta: float) -> void:
+	### Any game state updates go here
 	if Globals.clickedCard:
-		Globals.clickedCard.flip()
+		Globals.clickedCard.toggleSelect()
 		Globals.clickedCard = null
 		
 	if Globals.unhoveredCard:
@@ -79,25 +79,31 @@ func _process(delta: float) -> void:
 			Preview.setText("")
 			Globals.unhoveredCard = null
 			
-	
-func draw_card() -> void:
+	### Any display updates go hear
+	draw_board()
+	draw_hand()
+			
+func draw_from_deck() -> void:
 	var drawn_card = deck.pop_back()
 	player_hand.push_back(drawn_card)
 
-func update_hand_visualization() -> void:
-	# TODO: there's surely a better way to do this than deleting and re-creating them all
-	for child in Hand.get_children():
-		Hand.remove_child(child)
-		child.queue_free()
-		
-	var card_offset_x = -((CARD_WIDTH*BOARD_SCALE+SPACE_BETWEEN_CARDS)*len(player_hand)) / 2  + CARD_WIDTH*BOARD_SCALE/2
+func draw_board() -> void:
+	# create board
+	for r in range(BOARD_HEIGHT):
 
+		for c in range(BOARD_WIDTH):
+			var myCard = board_state[r][c]
+	
+func draw_hand() -> void:
 	for i in range(len(player_hand)):
+		var card_offset_x = -((CARD_WIDTH_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*len(player_hand)) / 2  + CARD_WIDTH_PX*BOARD_SCALE/2
+		
 		var myCard = instancedCard.instantiate()
 		Hand.add_child(myCard)
 		myCard.position = Vector2(
-			(CARD_WIDTH*BOARD_SCALE+SPACE_BETWEEN_CARDS)*i + card_offset_x,
+			(CARD_WIDTH_PX*BOARD_SCALE+SPACE_BETWEEN_CARDS_PX)*i + card_offset_x,
 			0)
 		myCard.scale = Vector2(BOARD_SCALE, BOARD_SCALE)
 		
 		myCard.setText(ALL_CARDS[player_hand[i]]["Name"])
+	
